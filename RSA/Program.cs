5750366992da0c7ff047fd1b1cbc1d69543fe200
张文杰 <746342384@qq.com>
2022-03-16 23:02:59 +0800

    111

--- RSA/Program.cs
+++ RSA/Program.cs
@@ -7,9 +7,7 @@ namespace RSA
         static void Main(string[] args)
         {
             RSAHelper.RsaKey(out RSAHelper.PrivateKey, out RSAHelper.PublicKey);
-            string data = @"RSA 是常用的非对称加密算法。最近使用时却出现了“不正确的长度”的异常，研究发现是由于待加密的数据超长所致。
-　　                    .NET Framework 中提供的 RSA 算法规定：
-　　                    待加密的字节数不能超过密钥的长度值除以 8 再减去 11（即：RSACryptoServiceProvider.KeySize / 8 - 11），而加密后得到密文的字节数，正好是密钥的长度值除以 8（即：RSACryptoServiceProvider.KeySize / 8）。
+            string data = @"待加密的字节数不能超过密钥的长度值除以 8 再减去 11（即：RSACryptoServiceProvider.KeySize / 8 - 11），而加密后得到密文的字节数，正好是密钥的长度值除以 8（即：RSACryptoServiceProvider.KeySize / 8）。
 　　                    所以，如果要加密较长的数据，则可以采用分段加解密的方式，实现方式如下：";
             string encryptStr = RSAHelper.Encrypt(data);
             Console.WriteLine("加密后结果：" + encryptStr);
